7a2e132750d31924e3f092f356e92d232754dadb
Lorenzo Furrer <>
2023-12-18 23:20:50 +0000

    store slider values in cookies

--- src/components/VisuallySimilarKanjiComponent.js
+++ src/components/VisuallySimilarKanjiComponent.js
@@ -3,26 +3,29 @@
 import { Col, Row, Form, Button } from 'react-bootstrap';
 import MultiRangeSliderComponent from './MultiRangeSlider/MultiRangeSliderComponent';
 import { LoadingSpinner } from './LoadingSpinner';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getCookie, setCookie } from 'cookies-next';
 
 export const VisuallySimilarKanji = (props) => {
 
     const [loading, setLoading] = useState(false);
 
-    let visMinValue = loadfromLocalStorage('visMinValue', 21);
-    let visMaxValue = loadfromLocalStorage('visMaxValue', 41);
+    const [visMinValue, setVisMinValue] = useState(Number.NaN);
+    const [visMaxValue, setVisMaxValue] = useState(Number.NaN);
 
-    function loadfromLocalStorage(localStorageKey, defaultvalue) {
-        const storedValue = JSON.parse(localStorage.getItem(localStorageKey));
-        return storedValue !== undefined ? storedValue : defaultvalue;
-    }
+    useEffect(() => {
+        const storedMin = Number(getCookie('visMinValue'));
+        const storedMax = Number(getCookie('visMaxValue'));
+        setVisMinValue(!Number.isNaN(storedMin) ? storedMin : 10);
+        setVisMaxValue(!Number.isNaN(storedMax) ? storedMax : 40);
+    }, []);
 
 
     function handleMultiRangeSliderChange({ min, max }) {
-        visMinValue = min;
-        localStorage.setItem('visMinValue', visMinValue);
-        visMaxValue = max;
-        localStorage.setItem('visMaxValue', visMaxValue);
+        setVisMinValue(min);
+        setCookie('visMinValue', min, { sameSite: true });
+        setVisMaxValue(max);
+        setCookie('visMaxValue', max, { sameSite: true });
         //console.log(`min = ${min}, max = ${max}`);
     }
 
@@ -43,13 +46,14 @@ export const VisuallySimilarKanji = (props) => {
                             Level Range:
                         </Col>
                         <Col className="col-8">
-                            <MultiRangeSliderComponent
-                                min={1}
-                                max={60}
-                                onChange={handleMultiRangeSliderChange}
-                                minStartValue={visMinValue}
-                                maxStartValue={visMaxValue}
-                            />
+                            {(!Number.isNaN(visMinValue) && !Number.isNaN(visMaxValue)) &&
+                                <MultiRangeSliderComponent
+                                    min={1}
+                                    max={60}
+                                    onChange={handleMultiRangeSliderChange}
+                                    minStartValue={visMinValue}
+                                    maxStartValue={visMaxValue}
+                                />}
                         </Col>
                         <Col className='col-2 p-2 d-flex flex-column'>
 
