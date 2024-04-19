d92aa0f022a209931104039e1d9c6d59fe474671
Lorenzo Furrer <>
2024-04-19 19:22:19 +0100

    configure domain based on environment

--- src/app/auth/config/appInfo.js
+++ src/app/auth/config/appInfo.js
@@ -2,8 +2,8 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "Japanese Reviews",
-  apiDomain: "http://localhost:3000",
-  websiteDomain: "http://localhost:3000",
+  apiDomain: process.env.WEBSITE_DOMAIN,
+  websiteDomain: process.env.WEBSITE_DOMAIN,
   apiBasePath: "/api/auth",
   websiteBasePath: "/auth"
 }
