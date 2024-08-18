38653587f7991b2901cc92190eb6176b626d33e1
Lorenzo Furrer <>
2024-08-18 11:06:20 +0100

    reenabled Supertokens for dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -9,124 +9,123 @@ import { useQuizContext } from '@/app/context/quizContext';
 import PendingReviewsComponent from '@/app/components/PendingReviewsComponent';
 import VisuallySimilarKanji from '@/app/components/VisuallySimilarKanjiComponent';
 import FlashcardSettings from '@/app/components/FlashcardSettingsComponent';
-// import { SessionAuth } from "supertokens-auth-react/recipe/session"
+import { SessionAuth } from "supertokens-auth-react/recipe/session"
 
 
 export default function Dashboard() {
 
-  const { setPromptSet, setReviewMode, reviewSet } = useQuizContext();
+    const { setPromptSet, setReviewMode, reviewSet } = useQuizContext();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  async function handleSetSelection(event) {
-    event.preventDefault();
+    async function handleSetSelection(event) {
+        event.preventDefault();
 
-    const dataOption = event.target.getAttribute('data-option');
-    const selectedLevel = Number(event.target.getAttribute('data-selected-level'));
-    const kanjiSetSelected = event.target.getAttribute('data-kanjiset-selected') === "true"
-    const vocabularySetSelected = event.target.getAttribute('data-vocabularyset-selected') === "true"
-    const guessMeaningSelected = event.target.getAttribute('data-guess-meaning-selected') === "true";
-    const guessReadingSelected = event.target.getAttribute('data-guess-reading-selected') === "true";
-    const guessKanjiSelected = event.target.getAttribute('data-guess-kanji-selected') === "true";
+        const dataOption = event.target.getAttribute('data-option');
+        const selectedLevel = Number(event.target.getAttribute('data-selected-level'));
+        const kanjiSetSelected = event.target.getAttribute('data-kanjiset-selected') === "true"
+        const vocabularySetSelected = event.target.getAttribute('data-vocabularyset-selected') === "true"
+        const guessMeaningSelected = event.target.getAttribute('data-guess-meaning-selected') === "true";
+        const guessReadingSelected = event.target.getAttribute('data-guess-reading-selected') === "true";
+        const guessKanjiSelected = event.target.getAttribute('data-guess-kanji-selected') === "true";
 
-    let promptSetResponse = await (await fetch('/api/quiz/prompts', {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        dataOption: dataOption,
-        selectedLevel: selectedLevel,
-        kanjiSetSelected: kanjiSetSelected,
-        vocabularySetSelected: vocabularySetSelected,
-        guessMeaningSelected: guessMeaningSelected,
-        guessReadingSelected: guessReadingSelected,
-        guessKanjiSelected: guessKanjiSelected,
-        reviewSetElementIds: reviewSet.map(review => review.element_id)
-      })
-    })).json();
+        let promptSetResponse = await (await fetch('/api/quiz/prompts', {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                dataOption: dataOption,
+                selectedLevel: selectedLevel,
+                kanjiSetSelected: kanjiSetSelected,
+                vocabularySetSelected: vocabularySetSelected,
+                guessMeaningSelected: guessMeaningSelected,
+                guessReadingSelected: guessReadingSelected,
+                guessKanjiSelected: guessKanjiSelected,
+                reviewSetElementIds: reviewSet.map(review => review.element_id)
+            })
+        })).json();
 
-    setReviewMode(dataOption === "review");
-    setPromptSet(promptSetResponse);
-    router.push('/quiz');
+        setReviewMode(dataOption === "review");
+        setPromptSet(promptSetResponse);
+        router.push('/quiz');
 
-  }
+    }
 
-  async function handleLevelSelection(minLevel, maxLevel) {
+    async function handleLevelSelection(minLevel, maxLevel) {
 
-    const fullKanjiSetResponse = await (await fetch('/api/quiz/prompts', {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        dataOption: "full-kanji",
-        kanjiSetSelected: true,
-        guessMeaningSelected: true,
-        guessReadingSelected: true
-      })
-    })).json();
+        const fullKanjiSetResponse = await (await fetch('/api/quiz/prompts', {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                dataOption: "full-kanji",
+                kanjiSetSelected: true,
+                guessMeaningSelected: true,
+                guessReadingSelected: true
+            })
+        })).json();
 
-    const levelSet = fullKanjiSetResponse
-      .filter(kanji => minLevel <= kanji['data']['level'] && kanji['data']['level'] <= maxLevel)
-      .filter(kanji => kanji['data']['visually_similar_subject_ids'].length > 0)
+        const levelSet = fullKanjiSetResponse
+            .filter(kanji => minLevel <= kanji['data']['level'] && kanji['data']['level'] <= maxLevel)
+            .filter(kanji => kanji['data']['visually_similar_subject_ids'].length > 0)
 
-    // fetch 'visually_similar_subject_ids' in other levels
-    const visuallySimilarKanjis = [...new Set(levelSet.flatMap(kanji => kanji['data']['visually_similar_subject_ids']))]
-      .flatMap(kanjiId => fullKanjiSetResponse.filter(kanji => kanji['id'] === kanjiId));
+        // fetch 'visually_similar_subject_ids' in other levels
+        const visuallySimilarKanjis = [...new Set(levelSet.flatMap(kanji => kanji['data']['visually_similar_subject_ids']))]
+            .flatMap(kanjiId => fullKanjiSetResponse.filter(kanji => kanji['id'] === kanjiId));
 
-    levelSet.push(...visuallySimilarKanjis)
-    // console.log('Visually Similar Kanji set: ', levelSet);
+        levelSet.push(...visuallySimilarKanjis)
+        // console.log('Visually Similar Kanji set: ', levelSet);
 
-    setPromptSet(levelSet);
-    router.push('/quiz');
-  }
+        setPromptSet(levelSet);
+        router.push('/quiz');
+    }
 
-  async function handleFlashcardFormSubmission(event) {
-    event.preventDefault();
+    async function handleFlashcardFormSubmission(event) {
+        event.preventDefault();
 
-    const selectedLevel = Number(event.target.getAttribute('data-selected-level'));
+        const selectedLevel = Number(event.target.getAttribute('data-selected-level'));
 
-    let promptSetResponse = await (await fetch('/api/quiz/prompts', {
-      method: 'post',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        dataOption: 'level',
-        selectedLevel: selectedLevel,
-        kanjiSetSelected: true,
-        vocabularySetSelected: true,
-        guessMeaningSelected: true,
-      })
-    })).json();
+        let promptSetResponse = await (await fetch('/api/quiz/prompts', {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                dataOption: 'level',
+                selectedLevel: selectedLevel,
+                kanjiSetSelected: true,
+                vocabularySetSelected: true,
+                guessMeaningSelected: true,
+            })
+        })).json();
 
-    setPromptSet(promptSetResponse);
-    router.push('/flashcards');
+        setPromptSet(promptSetResponse);
+        router.push('/flashcards');
 
-  }
+    }
 
-  return (
-    // Enable SessionAuth to protect route
-    // <SessionAuth>
-      <SSRProvider>
-        <Container fluid className='App'>
-          <Row>
-            <Col className='AppBody'>
-              <Row className='justify-content-center'>
-                <Col className='col-6'>
-                  <SelectSettings handleSetSelection={handleSetSelection} />
-                  <VisuallySimilarKanji handleLevelSelection={handleLevelSelection} />
-                  <FlashcardSettings handleFlashcardFormSubmission={handleFlashcardFormSubmission} />
-                </Col>
-                <Col className='col-3'>
-                  <PendingReviewsComponent handleSetSelection={handleSetSelection} />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </SSRProvider>
-    // </SessionAuth>
-  )
+    return (
+        <SessionAuth>
+            <SSRProvider>
+                <Container fluid className='App'>
+                    <Row>
+                        <Col className='AppBody'>
+                            <Row className='justify-content-center'>
+                                <Col className='col-6'>
+                                    <SelectSettings handleSetSelection={handleSetSelection} />
+                                    <VisuallySimilarKanji handleLevelSelection={handleLevelSelection} />
+                                    <FlashcardSettings handleFlashcardFormSubmission={handleFlashcardFormSubmission} />
+                                </Col>
+                                <Col className='col-3'>
+                                    <PendingReviewsComponent handleSetSelection={handleSetSelection} />
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </SSRProvider>
+        </SessionAuth>
+    )
 }
