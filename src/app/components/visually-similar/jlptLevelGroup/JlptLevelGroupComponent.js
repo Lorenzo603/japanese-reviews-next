fea0aa4600b1cd8100dd2ddbd22c6e597f10b4f4
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-08-24 21:19:55 +0100

    style jlpt pages

--- src/app/components/visually-similar/jlptLevelGroup/JlptLevelGroupComponent.js
+++ src/app/components/visually-similar/jlptLevelGroup/JlptLevelGroupComponent.js
@@ -1,4 +1,5 @@
 import { getDictionary } from '@/app/components/backend/DictionaryLoaderComponent';
+import Link from 'next/link';
 
 export const JlptLevelGroupComponent = async (props) => {
 
@@ -8,16 +9,32 @@ export const JlptLevelGroupComponent = async (props) => {
 
     return (
         <section>
-            <h1>JLPT Level {props.jlptLevelNumber}</h1>
-            <ul>
-                {kanjiList.map(kanji =>
-                    <li key={kanji["id"]}>
-                        <a className="japanese-font" href={`/visually-similar/kanji/${kanji["id"]}`}>
-                            {kanji["data"]["slug"]}
-                        </a>
-                    </li>
-                )}
-            </ul>
+            <div className='p-4'>
+                <h1>JLPT Level {props.jlptLevelNumber}</h1>
+                <ul className='flex flex-row flex-wrap m-0 p-0 gap-2'>
+                    {kanjiList.map(kanji =>
+                        <Link
+                            className="no-underline bg-pink-500 text-white 
+                                border border-pink-500 rounded-md
+                                p-2
+                                hover:bg-pink-600"
+                            href={`/visually-similar/kanji/${kanji["id"]}`}
+                        >
+                            <li key={kanji["id"]}>
+                                <div className='flex flex-col'>
+                                    <div className='japanese-font text-5xl flex justify-center'>
+                                        {kanji["data"]["slug"]}
+                                    </div>
+                                    <div className='flex justify-center pt-2'>
+                                        {kanji["data"]["meanings"][0]["meaning"]}
+                                    </div>
+
+                                </div>
+                            </li>
+                        </Link>
+                    )}
+                </ul>
+            </div>
 
         </section>
     );
