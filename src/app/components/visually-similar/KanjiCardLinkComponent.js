035653b3dd9c845a8c819dbe6d0cd3b3e249361f
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-08-25 12:35:17 +0100

    added kanji cardcomponent

---
+++ src/app/components/visually-similar/KanjiCardLinkComponent.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+export const KanjiCardLinkComponent = async (props) => {
+
+    return (
+        <li key={props.kanjiId} className='bg-pink-500 
+                    border border-pink-500 rounded-md
+                    p-2
+                    hover:bg-pink-600'>
+            <Link className="no-underline text-white"
+                href={`/visually-similar/kanji/${props.kanjiId}`}
+            >
+                <div className='flex flex-col w-24'>
+                    <div className='japanese-font text-5xl flex justify-center text-center'>
+                        {props.kanjiSlug}
+                    </div>
+                    <div className='flex justify-center text-center pt-2'>
+                        {props.kanjiMeaning}
+                    </div>
+
+                </div>
+            </Link>
+        </li>
+    );
+}
+
+export default KanjiCardLinkComponent;
