103df7bbfd353051d80e1158921a57ab519919e8
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-08-27 20:24:44 +0100

    User icon on sign out

--- src/app/components/buttons/login/LoginButton.js
+++ src/app/components/buttons/login/LoginButton.js
@@ -25,7 +25,13 @@ export const LoginButton = () => {
     return (
         <div>
             {isAuthenticated ? (
-                <button onClick={handleSignOut}>Sign Out</button>
+                <button onClick={handleSignOut}>
+                    <div className="bg-pink-50 rounded-full border-4 border-pink-300 p-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                        </svg>
+                    </div>
+                </button>
             ) : (
                 <Link className="line-clamp-1 flex items-center justify-center gap-2 rounded-md 
                     bg-pink-500 px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm 
