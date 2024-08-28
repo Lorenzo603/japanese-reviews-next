a06b49eb4cb7943206bdaea3541a5d5658aa6a2c
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-08-29 00:21:04 +0100

    Added segmented button control and Toggle

--- src/app/(visually-similar)/visually-similar/review-settings/page.js
+++ src/app/(visually-similar)/visually-similar/review-settings/page.js
@@ -7,11 +7,29 @@ export default function VisuallySimilarReviewSettings() {
 
     const router = useRouter();
 
-    const { setPromptSet, guessKanji, setGuessKanji, 
-        guessMeaning, 
-        multichoiceInput, setMultichoiceInput, 
+    const { setPromptSet, guessKanji, setGuessKanji,
+        guessMeaning,
+        multichoiceInput, setMultichoiceInput,
         typingInput,
         quickMode, setQuickMode } = useVisuallySimilarQuizContext();
+
+
+    const inactiveTabClassName = "bg-transparent";
+    const activeTabClassName = "rounded-md bg-white shadow";
+
+    const handleSegmentControlClick = (buttonId) => {
+        const buttons = document.getElementById("vis-sim-guess-mode-segment-control").children;
+        // Remove active styles from all buttons
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove(...activeTabClassName.split(/\s+/));
+            buttons[i].classList.add(...inactiveTabClassName.split(/\s+/));
+        };
+
+        const button = document.getElementById(buttonId);
+        // Set the selected button as active
+        button.classList.add(...activeTabClassName.split(/\s+/));
+        button.classList.remove(...inactiveTabClassName.split(/\s+/));
+    }
 
     const handleLevelNumberClick = async (selectedLevel) => {
 
@@ -44,12 +62,22 @@ export default function VisuallySimilarReviewSettings() {
                     <div className="max-w-2xl py-4">
                         <section>
                             <h1 className="text-2xl">Review Settings</h1>
-                            <p>Try it now</p>
                             <div className="flex flex-col">
                                 <div className="flex flex-row">
                                     <div>
-                                        Guess Kanji:
+                                        Guess Mode:
                                     </div>
+                                    <div id="vis-sim-guess-mode-segment-control" className="inline-flex h-9 w-full items-baseline justify-start rounded-lg bg-gray-100 p-1 sm:w-auto">
+                                        <button id="guess-mode-kanji" type="button" aria-disabled="false" onClick={() => handleSegmentControlClick("guess-mode-kanji")}
+                                            className="group inline-flex items-center justify-center whitespace-nowrap py-2 align-middle font-semibold transition-all duration-300 ease-in-out disabled:cursor-not-allowed stroke-blue-700 min-w-[32px] gap-1.5 text-xs disabled:stroke-slate-400 disabled:text-slate-400 hover:stroke-blue-950 hover:text-blue-950 h-7 text-slate-950 w-full rounded-md bg-white px-3 drop-shadow sm:w-auto">
+                                            <span>Guess Kanji</span>
+                                        </button>
+                                        <button id="guess-mode-meaning" type="button" aria-disabled="false" onClick={() => handleSegmentControlClick("guess-mode-meaning")}
+                                            className="group inline-flex items-center justify-center whitespace-nowrap rounded-lg py-2 align-middle font-semibold transition-all duration-300 ease-in-out disabled:cursor-not-allowed stroke-blue-700 min-w-[32px] gap-1.5 text-xs disabled:stroke-slate-400 disabled:text-slate-400 hover:stroke-blue-950 hover:text-blue-950 h-7 w-full bg-transparent px-3 text-slate-600 sm:w-auto">
+                                            <span>Guess Meaning</span>
+                                        </button>
+                                    </div>
+
                                     <div>
                                         <input type="checkbox" checked={guessKanji} onChange={() => setGuessKanji(!guessKanji)} />
                                     </div>
@@ -64,9 +92,23 @@ export default function VisuallySimilarReviewSettings() {
                                 </div>
                                 <div className="flex flex-row">
                                     <div>
-                                        Quick Mode:
+                                        Enable Quick Mode:
                                     </div>
                                     <div>
+                                        <div className="inline-flex items-center justify-center">
+                                            <label className="font-medium transition-colors duration-300 ease-in-out peer-disabled:opacity-70 text-xs flex items-center">
+                                                <div className="relative">
+                                                    <input role="switch" id="switch-1" className="peer sr-only" aria-label="Checked" aria-checked="true" type="checkbox" onChange={() => setQuickMode(!quickMode)} name="switch" />
+                                                    <div
+                                                        className="block cursor-pointer rounded-full border border-slate-300 bg-slate-50 transition duration-300 peer-checked:border-blue-700 peer-checked:bg-blue-700 peer-disabled:cursor-not-allowed peer-disabled:border-slate-100 peer-disabled:bg-slate-100 h-6 w-12">
+                                                    </div>
+                                                    <div
+                                                        className="absolute top-0.5 z-10 cursor-pointer rounded-full border border-slate-50 bg-blue-700 transition duration-300 peer-checked:translate-x-5 peer-checked:border-blue-700 peer-checked:bg-white peer-disabled:cursor-not-allowed peer-disabled:border-slate-100 peer-disabled:bg-slate-400 left-[3px] size-5 peer-checked:left-[5px]">
+                                                    </div>
+                                                </div>
+                                                <span className="ml-2 cursor-pointer whitespace-nowrap text-xs font-medium leading-none text-black">Checked</span>
+                                            </label>
+                                        </div>
                                         <input type="checkbox" checked={quickMode} onChange={() => setQuickMode(!quickMode)} />
                                     </div>
                                 </div>
