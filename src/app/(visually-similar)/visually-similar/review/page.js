866e6d88d4e7746a88023b6ea37740c43d7d85ca
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-08-28 10:29:17 +0100

    quiz guess kanji multiselect skeleton

--- src/app/(visually-similar)/visually-similar/review/page.js
+++ src/app/(visually-similar)/visually-similar/review/page.js
@@ -1,10 +1,26 @@
 'use client'
 
 import { useVisuallySimilarQuizContext } from "@/app/context/visuallySimilarQuizContext";
+import { useState } from "react";
 
 export default function VisuallySimilarReview() {
 
     const { promptSet, multichoiceInput, typingInput } = useVisuallySimilarQuizContext();
+
+    const [currentPromptIndex, setCurrentPromptIndex] = useState(0);
+    const [currentPrompt, setCurrentPrompt] = useState(promptSet[currentPromptIndex]);
+
+    const handleUserAnswer = async (answer) => {
+        if (answer === currentPrompt["correctAnswer"]) {
+            console.log("Correct answer!");
+        } else {
+            console.log("Wrong answer");
+        }
+        if (currentPromptIndex < promptSet.length - 1) {
+            setCurrentPromptIndex(currentPromptIndex + 1);
+            setCurrentPrompt(promptSet[currentPromptIndex + 1]);
+        }
+    } 
 
     return (
         <main>
@@ -12,10 +28,19 @@ export default function VisuallySimilarReview() {
                 <div className="mx-auto max-w-7xl p-6">
                     <div className="max-w-2xl py-4">
                         <section>
-                            <p>{promptSet[0]["data"]["slug"]}</p>
-                            <p className="japanese-font">{promptSet[1]["data"]["slug"]}</p>
-                            <p>{promptSet[2]["data"]["slug"]}</p>
-                       
+                            <div className="text-2xl">{currentPrompt["prompt"]}</div>
+                            <div>
+                                {
+                                    currentPrompt["answers"].map(answer => {
+                                        return <div key={answer}>
+                                            <button className='bg-red-500 text-white rounded-md p-2 hover:bg-red-600 w-full'
+                                                onClick={() => handleUserAnswer(answer)}>
+                                                <span className="japanese-font">{answer}</span>
+                                            </button>
+                                        </div>
+                                    })
+                                }
+                            </div>
                         </section>
                     </div>
                 </div>
--- src/app/api/visually-similar/quiz/prompts/route.js
+++ src/app/api/visually-similar/quiz/prompts/route.js
@@ -7,7 +7,30 @@ export async function POST(request) {
     const selectedLevel = reqJson.selectedLevel;
 
     const fullKanjiDictionary = await getDictionary('kanji_full_reduced');
+    let promptSet = fullKanjiDictionary
+        .filter(kanji => kanji['data']['level'] === selectedLevel)
+        .filter(kanji => kanji['data']['visually_similar_subject_ids'].length > 0)
+        .map(kanji => ({
+                prompt: kanji['data']['meanings'][0]['meaning'], 
+                answers: buildAnswers(kanji, fullKanjiDictionary),
+                correctAnswer: kanji['data']['slug'],
+            })
+        );
 
-    return NextResponse.json(fullKanjiDictionary
-        .filter(kanji => kanji['data']['level'] === selectedLevel));
+    promptSet = shuffle(promptSet);
+    return NextResponse.json(promptSet);
+}
+
+function buildAnswers(kanji, fullKanjiDictionary) {
+    const visuallySimilarIds = kanji['data']['visually_similar_subject_ids']
+    const answers = [kanji['data']['slug']]
+    answers.push(...visuallySimilarIds.map(id => fullKanjiDictionary.find(k => k['id'] === id)['data']['slug']))
+    return shuffle(answers)
+}
+
+function shuffle(unshuffled) {
+    return unshuffled
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
 }
