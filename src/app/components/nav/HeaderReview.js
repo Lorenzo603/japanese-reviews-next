f32599f7eb3562ffb4519817777530e66e8d6912
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-01 19:09:27 +0100

    style

--- src/app/components/nav/HeaderReview.js
+++ src/app/components/nav/HeaderReview.js
@@ -78,8 +78,8 @@ const HeaderReview = () => {
 
 
                         <ul className="flex-grow">
-                            <li className="py-2">
-                                <button className="text-gray-800 hover:text-gray-600" onClick={toggleFocusMode}>
+                            <li className="flex items-center text-xl hover:bg-pink-50">
+                                <button className="w-full text-gray-800 py-2" onClick={toggleFocusMode}>
                                     <div className="flex flex-row gap-x-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
@@ -89,8 +89,8 @@ const HeaderReview = () => {
                                 </button>
                             </li>
                             <div aria-hidden="false" className="w-full h-1"><hr className="block bg-rim w-full h-1" /></div>
-                            <li className="py-2">
-                                <button className="text-gray-800 hover:text-gray-600" onClick={endSession}>
+                            <li className="flex items-center text-xl hover:bg-pink-50">
+                                <button className="w-full text-gray-800 py-2" onClick={endSession}>
                                     <div className="flex flex-row gap-x-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
