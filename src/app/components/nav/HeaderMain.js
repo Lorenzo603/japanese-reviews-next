d4310fc75ea2d0abe31070e5138bd22fbdaf8785
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-01 20:53:48 +0100

    Added icon and styling to header link

--- src/app/components/nav/HeaderMain.js
+++ src/app/components/nav/HeaderMain.js
@@ -18,17 +18,20 @@ const HeaderMain = () => {
                         </div>
                     </Link>
                 </div>
-                <div className="flex flex-1 items-center justify-end gap-x-4">
+                <div className="flex items-center justify-end gap-x-4">
                     <div className="flex items-center">
                         <Link
                             className="no-underline 
                             font-bold text-blue-900
-                            items-baseline 
-                            hidden md:inline 
-                            rounded-md px-2 py-2.5
+                            items-center 
+                            hidden md:flex 
+                            rounded-md gap-x-2 px-2 py-2.5
                             hover:bg-slate-50"
                             href="/visually-similar/review-settings">
-                            Start Reviews
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                            </svg>
+                            <span>Start Reviews</span>
                         </Link>
                     </div>
                     <div className="flex items-center">
