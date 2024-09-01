694b4a9be8ec9aa4d6f4c5181472e6ec7206b75c
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-01 20:22:34 +0100

    style SearchBox

--- src/app/(visually-similar)/page.js
+++ src/app/(visually-similar)/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import SearchComponent from "../components/search/SearchComponent";
+import SvgArrows from "./SvgArrows";
 
 export default function Home() {
 
@@ -12,33 +13,29 @@ export default function Home() {
   return (
     <main>
       <div className="w-full">
-        <div className="mx-auto max-w-7xl p-6">
-          <section>
-            <div>
-              <h1 className="sr-only">Search</h1>
-              <div className="flex-1 sm:flex justify-center pb-4">
-                <div>
-                  <SearchComponent />
-                </div>
+        <section>
+          <div>
+            <h1 className="sr-only">Search</h1>
+            <div className="flex justify-center p-4">
+              <div>
+                <SearchComponent />
               </div>
             </div>
-          </section>
-
+          </div>
+        </section>
+        <div className="mx-auto max-w-7xl p-6">
           <div className="max-w-2xl py-4">
             <section>
               <h1 className="sr-only text-2xl">Reviews Settings</h1>
               <Link className="line-clamp-1 flex items-center justify-center gap-2 rounded-md 
                     bg-pink-500 px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm 
-                    hover:bg-pink-700 no-underline" 
-                    href="/visually-similar/review-settings">
+                    hover:bg-pink-700 no-underline"
+                href="/visually-similar/review-settings">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                 </svg>
                 <span>Start reviews</span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                  <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                  <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                </svg>
+                <SvgArrows />
               </Link>
             </section>
           </div>
@@ -51,10 +48,7 @@ export default function Home() {
                     bg-pink-500 px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm 
                     hover:bg-pink-700 no-underline"
                   href="/visually-similar/jlpt-level-5">JLPT 5
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                    <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                    <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                  </svg>
+                  <SvgArrows />
                 </Link>
               </div>
               <div className="pt-2 pb-2">
@@ -62,10 +56,7 @@ export default function Home() {
                     bg-pink-500 px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm 
                     hover:bg-pink-700 no-underline"
                   href="/visually-similar/jlpt-level-4">JLPT 4
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                    <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                    <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                  </svg>
+                  <SvgArrows />
                 </Link>
               </div>
               <div className="pt-2 pb-2">
@@ -73,10 +64,7 @@ export default function Home() {
                     bg-pink-500 px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm 
                     hover:bg-pink-700 no-underline"
                   href="/visually-similar/jlpt-level-3">JLPT 3
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                    <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                    <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                  </svg>
+                  <SvgArrows />
                 </Link>
               </div>
               <div className="pt-2 pb-2">
@@ -84,10 +72,7 @@ export default function Home() {
                     bg-pink-500 px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm 
                     hover:bg-pink-700 no-underline"
                   href="/visually-similar/jlpt-level-2">JLPT 2
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                    <path fillRule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                    <path fillRule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
-                  </svg>
+                  <SvgArrows />
                 </Link>
               </div>
             </section>
