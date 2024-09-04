81670d4800694c52713cf48f27f75dd9c4f55230
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-04 17:23:58 +0100

    Fix modal display on top of toggle bug

--- src/app/components/modals/ContinueModal.js
+++ src/app/components/modals/ContinueModal.js
@@ -4,7 +4,7 @@ const ContinueModal = (props) => {
 
     return (
         props.show &&
-        <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="relative z-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
             {/* Background backdrop, show/hide based on modal state.
 
             Entering: "ease-out duration-300"
@@ -15,7 +15,7 @@ const ContinueModal = (props) => {
             To: "opacity-0" */}
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
-            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     {/* Modal panel, show/hide based on modal state.
 
