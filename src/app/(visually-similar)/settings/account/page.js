0cc87adcc00392d2d3e20b6db349395bf2537739
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-13 20:55:35 +0100

    Added Update Email logic

--- src/app/(visually-similar)/settings/account/page.js
+++ src/app/(visually-similar)/settings/account/page.js
@@ -10,6 +10,9 @@ export default function SettingsAccount() {
 
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     async function loadAccountSettings() {
@@ -40,7 +43,6 @@ export default function SettingsAccount() {
       }
 
       const resJson = await response.json();
-      // console.log('Account Settings from DB:', resJson);
       return resJson;
     } catch (error) {
       console.error('Error loading user settings:', error);
@@ -48,43 +50,134 @@ export default function SettingsAccount() {
     }
   }
 
+  const handleUpdateUsername = async () => {
+    try {
+      const response = await fetch('/api/visually-similar/settings/update-username', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ newUsername })
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error status: ${response.status}`);
+      }
+
+      const resJson = await response.json();
+      setUsername(resJson.username);
+      setNewUsername('');
+    } catch (error) {
+      console.error('Error updating username:', error);
+    }
+  };
+
+  const handleUpdateEmail = async () => {
+    try {
+      const response = await fetch('/api/visually-similar/settings/account/update-email', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ newEmail, password })
+      });
+      
+      const resJson = await response.json();
+      if (!response.ok) {
+        if (response.status === 400) {
+          alert(resJson.message);
+        }
+        return;
+      }
+
+      setEmail(resJson.email);
+      setNewEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error('Error updating email:', error);
+    }
+  };
 
   return (
     <SessionAuth accessDeniedScreen={AccessDeniedScreen}>
-
       <main>
         <div className="w-full">
-
           <div className="mx-auto max-w-7xl p-6 flex flex-col">
-            <h1 className="text-2xl">Profile</h1>
-            <section>
-              <h2 className="text-xl">Username</h2>
-              <div>{username}</div>
-              <div>Edit</div>
-
-            </section>
-            <section>
-              <h2 className="text-xl">Email</h2>
-              <div>{email}</div>
-              <div>Edit</div>
-
-            </section>
-            <section>
-              <h2 className="text-xl">Password</h2>
-              <Link href="/sign-in/reset-password">Reset Password</Link>
-
-            </section>
+            <h1 className="text-2xl mb-2">Profile</h1>
 
             <section>
-              <h2 className="text-xl">Profile Image</h2>
+              <div className='flex flex-col gap-2 mb-6'>
+                <h2 className="text-xl">Username</h2>
+                <p>Your username is <span className='font-bold'>{username}</span></p>
+                <div className="flex flex-row gap-2">
+                  <input
+                    type="text"
+                    value={newUsername}
+                    onChange={(e) => setNewUsername(e.target.value)}
+                    placeholder="Enter new username"
+                    className="placeholder:text-slate-400 text-slate-700 bg-slate-50 border border-slate-400 rounded p-2"
+                  />
+                  <button
+                    onClick={handleUpdateUsername}
+                    className="p-2 bg-pink-500 hover:bg-pink-700 text-white rounded"
+                  >
+                    Update username
+                  </button>
+                </div>
 
+              </div>
             </section>
 
+            <section>
+              <div className='flex flex-col gap-2 mb-6'>
+                <h2 className="text-xl">Email</h2>
+                <p>Your email address is <span className='font-bold'>{email}</span></p>
+                <div className="flex flex-row gap-2">
+                  <input
+                    type="email"
+                    value={newEmail}
+                    onChange={(e) => setNewEmail(e.target.value)}
+                    placeholder="Enter new email"
+                    className="placeholder:text-slate-400 text-slate-700 bg-slate-50 border border-slate-400 rounded p-2"
+                  />
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Confirm password"
+                    className="placeholder:text-slate-400 text-slate-700 bg-slate-50 border border-slate-400 rounded p-2"
+                  />
+                  <button
+                    onClick={handleUpdateEmail}
+                    className="p-2 bg-pink-500 hover:bg-pink-700 text-white rounded"
+                  >
+                    Update email
+                  </button>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <div className='flex flex-col gap-4 mb-6'>
+                <h2 className="text-xl">Password</h2>
+                <div>
+                  <Link href="/sign-in/reset-password"
+                    className="p-2 bg-pink-500 hover:bg-pink-700 text-white rounded">
+                    Reset Password
+                  </Link>
+                </div>
+              </div>
+            </section>
+
+            <section>
+              <div className='flex flex-col gap-2 mb-4'>
+                <h2 className="text-xl">Profile Image</h2>
+                {/* Profile Image handling can be added here */}
+              </div>
+            </section>
           </div>
-        </div >
-      </main >
-
+        </div>
+      </main>
     </SessionAuth>
-
-  )
+  );
 }
---
+++ src/app/api/visually-similar/settings/account/update-email/route.js
@@ -0,0 +1,69 @@
+import { NextResponse } from "next/server";
+import { withSession } from "supertokens-node/nextjs";
+import { ensureSuperTokensInit } from "@/app/(auth)/sign-in/config/backend.js";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
+
+ensureSuperTokensInit();
+
+export async function POST(request) {
+    return withSession(request, async (err, session) => {
+        if (err) {
+            console.error(err);
+            return NextResponse.json(err, { status: 500 });
+        }
+        if (!session) {
+            return new NextResponse("Authentication required", { status: 401 });
+        }
+
+        const reqJson = await request.json();
+
+        const email = reqJson.newEmail;
+        console.log(`Updating email to: ${email}`);
+        // Validate the input email
+        if (!isValidEmail(email)) {
+            // TODO: handle invalid email error
+            return NextResponse.json({
+                error: 'Internal Server Error',
+                message: 'Email is invalid.'
+            }, { status: 400 })
+        }
+
+        // Update the email
+        let resp = await EmailPassword.updateEmailOrPassword({
+            recipeUserId: session.getRecipeUserId(),
+            email: email,
+        });
+
+        if (resp.status === "OK") {
+            // send successfully updated email response
+            return NextResponse.json({
+                email: email,
+            }, { status: 200 })
+        }
+        if (resp.status === "EMAIL_ALREADY_EXISTS_ERROR") {
+            // handle error that email exists with another account.
+            return NextResponse.json({
+                error: 'Internal Server Error',
+                message: 'Email already exists.'
+            }, { status: 400 })
+        }
+        if (resp.status === "EMAIL_CHANGE_NOT_ALLOWED_ERROR") {
+            // This is possible if you have enabled account linking.
+            // See our docs for account linking to know more about this. 
+            // TODO: tell the user to contact support.
+            return NextResponse.json({
+                error: 'Internal Server Error',
+                message: 'Email change not allowed. Please contact support.'
+            }, { status: 400 })
+        }
+
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    });
+}
+
+function isValidEmail(email) {
+    let regexp = new RegExp(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    return regexp.test(email);
+}
