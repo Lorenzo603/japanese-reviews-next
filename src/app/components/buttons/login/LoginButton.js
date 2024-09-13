1363911acd952a5f53fe3bd3f990a96f12692957
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-13 18:32:46 +0100

    disabled Boring Avatars due to noisy defaultProps error

--- src/app/components/buttons/login/LoginButton.js
+++ src/app/components/buttons/login/LoginButton.js
@@ -40,9 +40,21 @@ export const LoginButton = () => {
         <div>
 
             {isAuthenticated ? (
-                username &&
+
                 <button className="flex items-center" onClick={toggleMenu}>
-                    <Avatar size={48} name={username} variant="beam" />
+                    {username
+                        ? <div className="bg-pink-50 rounded-full border-4 border-pink-300 p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg>
+                        </div>
+                        // TODO: Reactivate once defaultProps error is fixed <Avatar name={username} variant="beam" size={48} />
+                        : <div className="bg-pink-50 rounded-full border-4 border-pink-300 p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg>
+                        </div>
+                    }
                 </button>
             ) : (
                 <div>
