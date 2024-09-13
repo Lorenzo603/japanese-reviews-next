d58b10f068f1675f0ab909952ba036a577180b80
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-13 22:35:43 +0100

    added success alert and verification email when changing email

--- src/app/(visually-similar)/settings/account/page.js
+++ src/app/(visually-similar)/settings/account/page.js
@@ -14,6 +14,7 @@ export default function SettingsAccount() {
   const [newEmail, setNewEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   useEffect(() => {
     async function loadAccountSettings() {
@@ -71,6 +72,7 @@ export default function SettingsAccount() {
 
       setUsername(resJson.username);
       setNewUsername('');
+      setSuccessMessage("Username updated successfully.");
     } catch (error) {
       console.error('Error updating username:', error);
     }
@@ -94,9 +96,15 @@ export default function SettingsAccount() {
         return;
       }
 
+      if (resJson.status === 'VERIFICATION_EMAIL_SENT') {
+        setSuccessMessage(resJson.message);
+        return;
+      }
+
       setEmail(resJson.email);
       setNewEmail('');
       setPassword('');
+      setSuccessMessage("Email updated successfully.");
     } catch (error) {
       console.error('Error updating email:', error);
     }
@@ -120,6 +128,25 @@ export default function SettingsAccount() {
                 </div>
                 <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center 
                   justify-center h-8 w-8 border border-red-500" data-dismiss-target="#alert-2" aria-label="Close" onClick={() => setErrorMessage('')}>
+                  <span className="sr-only">Close</span>
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                  </svg>
+                </button>
+              </div>
+            }
+            {
+              successMessage &&
+              <div id="alert-3" className="flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 border border-green-500" role="alert">
+                <svg className="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span className="sr-only">Info</span>
+                <div className="ms-3 text-sm font-medium">
+                  {successMessage}
+                </div>
+                <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center
+                 justify-center h-8 w-8 border border-green-500" data-dismiss-target="#alert-3" aria-label="Close" onClick={() => setSuccessMessage('')}>
                   <span className="sr-only">Close</span>
                   <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
--- src/app/api/visually-similar/settings/account/update-email/route.js
+++ src/app/api/visually-similar/settings/account/update-email/route.js
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import { withSession } from "supertokens-node/nextjs";
 import { ensureSuperTokensInit } from "@/app/(auth)/sign-in/config/backend.js";
 import EmailPassword from "supertokens-node/recipe/emailpassword";
+import EmailVerification from "supertokens-node/recipe/emailverification";
+import {isEmailChangeAllowed} from "supertokens-node/recipe/accountlinking"
+import supertokens from "supertokens-node";
 
 ensureSuperTokensInit();
 
@@ -19,7 +22,7 @@ export async function POST(request) {
 
         const email = reqJson.newEmail;
         // console.log(`Updating email to: ${email}`);
-        
+
         // Validate the input email
         if (!isValidEmail(email)) {
             // handle invalid email error
@@ -28,6 +31,58 @@ export async function POST(request) {
                 message: 'Email is invalid.',
             }, { status: 400 })
         }
+
+        // TODO: Reenable Email verification
+        // // Then, we check if the email is verified for this user ID or not.
+        // // It is important to understand that SuperTokens stores email verification
+        // // status based on the user ID AND the email, and not just the email.
+        // let isVerified = await EmailVerification.isEmailVerified(session.getRecipeUserId(), email);
+        // if (!isVerified) {
+        //     if (!(await isEmailChangeAllowed(session.getRecipeUserId(), email, false))) {
+        //         // this can come here if you have enabled the account linking feature, and 
+        //         // if there is a security risk in changing this user's email.
+        //         return NextResponse.json({
+        //             error: 'Internal Server Error',
+        //             message: 'Email change not allowed. Please contact support.',
+        //         }, { status: 400 })
+        //     } 
+    
+        //     // Before sending a verification email, we check if the email is already
+        //     // being used by another user. If it is, we throw an error.
+        //     let user = await supertokens.getUser(session.getUserId());
+        //     if (!user) {
+        //         return NextResponse.json({
+        //             error: 'Internal Server Error',
+        //             message: 'User not found.',
+        //         }, { status: 400 })
+        //     }
+        //     for (let i = 0; i < user.tenantIds.length; i++) {
+        //         let usersWithSameEmail = await supertokens.listUsersByAccountInfo(user.tenantIds[i], {
+        //             email
+        //         });
+        //         for (let y = 0; y < usersWithSameEmail.length; y++) {
+        //             // Since one user can be shared across many tenants, we need to check if
+        //             // the email already exists in any of the tenants that belongs to this user.
+        //             let currUser = usersWithSameEmail[y];
+        //             if (currUser?.id !== session.getUserId()) {
+        //                 // TODO handle error, email already exists with another user.
+        //                 return NextResponse.json({
+        //                     error: 'Internal Server Error',
+        //                     message: 'Email already exists.',
+        //                 }, { status: 400 })
+        //             }
+        //         }
+        //     }
+    
+        //     // Now we create and send the email verification link to the user for the new email.
+        //     await EmailVerification.sendEmailVerificationEmail(session.getTenantId(), session.getUserId(), session.getRecipeUserId(), email);
+    
+        //     // TODO send successful response that email verification email sent.
+        //     return NextResponse.json({
+        //         status: 'VERIFICATION_EMAIL_SENT',
+        //         message: 'Verification email sent.',
+        //     }, { status: 200 })
+        // }
 
         // Update the email
         let resp = await EmailPassword.updateEmailOrPassword({
@@ -38,6 +93,7 @@ export async function POST(request) {
         if (resp.status === "OK") {
             // send successfully updated email response
             return NextResponse.json({
+                status: 'OK',
                 email: email,
             }, { status: 200 })
         }
