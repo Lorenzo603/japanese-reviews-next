4ed8c8924029ff4671d1b5d3c4cf1a1641c2ab01
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-19 00:31:13 +0100

    hide footer in focus mode

--- src/app/components/nav/FooterComponent.js
+++ src/app/components/nav/FooterComponent.js
@@ -1,15 +1,26 @@
+'use client'
+
+import { useVisuallySimilarQuizContext } from "@/app/context/visuallySimilarQuizContext";
+import { usePathname } from "next/navigation";
 
 const FooterComponent = () => {
-    return (
-        <footer className="bg-pink-700 text-slate-100 relative bottom-0 left-0 right-0">
-        <div className="flex">
-          <div className="container mx-auto flex max-w-7xl flex-col items-center justify-center gap-4 
+
+  const pathname = usePathname();
+
+  const { focusModeEnabled } = useVisuallySimilarQuizContext();
+
+  const showFooterContent = pathname !== '/visually-similar/review' || !focusModeEnabled;
+
+  return (
+    <footer className="bg-pink-700 text-slate-100 relative bottom-0 left-0 right-0">
+      <div className="flex">
+        <div className="container mx-auto flex max-w-7xl flex-col items-center justify-center gap-4 
              px-4 py-6">
-            <p className="text-sm">© {new Date().getFullYear()} Lorenzo Furrer. All rights reserved.</p>
-          </div>
+          {showFooterContent && <p className="text-sm">© {new Date().getFullYear()} Lorenzo Furrer. All rights reserved.</p>}
         </div>
-      </footer>
-    );
+      </div>
+    </footer>
+  );
 }
 
 export default FooterComponent;
