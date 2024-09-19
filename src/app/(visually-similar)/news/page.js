e633255a91e2d9c38e7b093094827d78708607ea
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-09-19 10:04:30 +0100

    styling a bit news section

--- src/app/(visually-similar)/news/page.js
+++ src/app/(visually-similar)/news/page.js
@@ -1,3 +1,4 @@
+import Breadcrumb from "@/app/components/breadcrumbs/Breadcrumb";
 import Link from "next/link";
 
 
@@ -6,26 +7,35 @@ export default function NewsPage() {
 
 
     return (
-        <main>
-            <div className="w-full">
-                <div className="mx-auto max-w-7xl p-6">
-                    <div className="max-w-2xl py-4">
+        <div className="w-full">
+            <div className="mx-auto max-w-7xl px-6 pb-6">
+                <Breadcrumb />
+                <main>
+                    <div className="max-w-2xl">
                         <section>
-                            <h1 className="text-2xl">News</h1>
+                            <h1 className="text-4xl text-pink-800 font-bold">News</h1>
                             <section>
-                                <h2>News Title</h2>
-                                <div>
-                                    <div>date</div>
-                                    <p>
-                                        Content
-                                    </p>
-                                </div>    
+                                <div className="py-4">
+                                    <div className="flex flex-row gap-4">
+                                        <h2 className="text-2xl font-bold pb-2">Welcome!</h2>
+                                        {/* <span>- date here</span> */}
+                                    </div>
+                                    <div>
+                                        <p>
+                                            Welcome to the site, you can find info here.
+                                        </p>
+                                        <p>
+                                            The code for this site is available on Github.
+                                        </p>
+                                    </div>
+                                </div>
                             </section>
                         </section>
+                        <hr />
                     </div>
-                </div>
+                </main>
             </div>
-        </main>
+        </div>
 
     )
 }
