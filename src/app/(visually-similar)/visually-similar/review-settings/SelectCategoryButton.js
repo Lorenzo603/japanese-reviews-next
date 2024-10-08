c3eb6743637ff2d390a788a26258048fc7c454d5
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-10-08 19:27:14 +0100

    style select category card

--- src/app/(visually-similar)/visually-similar/review-settings/SelectCategoryButton.js
+++ src/app/(visually-similar)/visually-similar/review-settings/SelectCategoryButton.js
@@ -4,11 +4,29 @@ export const SelectCategoryButton = (props) => {
     return (
         <button className={`
             ${props.isSelected ? "bg-green-400 border border-green-600" : "bg-pink-400 hover:bg-pink-600 border border-pink-600"} 
-            text-white rounded-md p-2 w-full h-full
+            text-white rounded-md p-2 w-full h-full min-h-32
             flex justify-center items-center
             `}
             onClick={props.handleSelectCategoryClick}>
-            {props.categoryName}
+
+            <div className="flex flex-col w-full h-full">
+                <div className="flex flex-row justify-between">
+                    <div>
+                        &#9917;
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                        </svg>
+
+                    </div>
+                </div>
+                <div className="flex grow items-center justify-center">
+                    <div className="text-lg">{props.categoryName}</div>
+                </div>
+
+                <div className="text-sm text-right mt-2">9 items</div>
+            </div>
         </button>
     )
 
