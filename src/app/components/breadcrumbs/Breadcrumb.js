e14afc5d4c2692056d533cd202cb502bd1fb4941
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2025-04-12 20:51:23 +0100

    fixed bug where jlpt link is null for kanjis that do not have jlpt category

--- src/app/components/breadcrumbs/Breadcrumb.js
+++ src/app/components/breadcrumbs/Breadcrumb.js
@@ -4,35 +4,39 @@ import Link from "next/link";
 
 export const Breadcrumb = (props) => {
 
-  if (!props.isKanjiPage) {
-    return (
-      <ul className="flex items-center py-4">
-        <li className="flex flex-row items-center">
-          <Link href="/" className="flex items-center gap-x-1">
-            <HomeIconSvg />
-            <span>Home</span>
-          </Link>
-        </li>
-      </ul>
-    )
-  }
+	if (!props.isKanjiPage) {
+		return (
+			<ul className="flex items-center py-4">
+				<li className="flex flex-row items-center">
+					<Link href="/" className="flex items-center gap-x-1">
+						<HomeIconSvg />
+						<span>Home</span>
+					</Link>
+				</li>
+			</ul>
+		)
+	}
 
-  return (
-    <ul className="flex items-center py-4">
-      <li className="flex flex-row items-center">
-        <Link href="/" className="flex items-center gap-x-1">
-          <HomeIconSvg />
-          <span>Home</span>
-        </Link>
-      </li>
-      <li className="flex flex-row items-center">
-        <Link href={`/visually-similar/jlpt-level-${props.jlptLevel}`} className="flex items-center">
-          <ArrowChevronRightSvg />
-          <span>JLPT {props.jlptLevel}</span>
-        </Link>
-      </li>
-    </ul>
-  )
+	return (
+		<ul className="flex items-center py-4">
+			<li className="flex flex-row items-center">
+				<Link href="/" className="flex items-center gap-x-1">
+					<HomeIconSvg />
+					<span>Home</span>
+				</Link>
+			</li>
+			{
+				props.jlptLevel &&
+				<li className="flex flex-row items-center">
+					<Link href={`/visually-similar/jlpt-level-${props.jlptLevel}`} className="flex items-center">
+						<ArrowChevronRightSvg />
+						<span>JLPT {props.jlptLevel}</span>
+					</Link>
+				</li>
+			}
+
+		</ul>
+	)
 };
 
 export default Breadcrumb;
